78eab15faa4af1ba2008a73332e15d490ca4526d
Piotr Przywieczerski <pprzywieczerski@speednet.pl>
2021-03-20 14:11:37 +0100

    Add glide.

---
+++ app/src/main/java/com/prenly/newsreader/MyAppGlideModule.java
@@ -0,0 +1,11 @@
+package com.prenly.newsreader;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+/**
+ * Created by Piotr Przywieczerski on 20.03.2021.
+ */
+
+@GlideModule
+public final class MyAppGlideModule extends AppGlideModule {}
